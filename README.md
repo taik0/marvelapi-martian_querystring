a4b9017cee9040732c43d6f4029257c0cc982c98
taik0 <suikogaiden@gmail.com>
2018-02-01 20:49:10 +0100

    Add martian modifier and README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# Marvel API martian querystring modifier
+
+
+This package is a (https://github.com/google/martian)martian modifier for
+authentication against the Marvel comics API.
+
+
+The procedure extracted from the Marvel API documentation:
+https://developer.marvel.com/documentation/authorization
+
+>Authentication for Server-Side Applications
+Server-side applications must pass two parameters in addition to the apikey parameter:
+ts - a timestamp (or other long string which can change on a request-by-request basis)
+hash - a md5 digest of the ts parameter, your private key and your public key (e.g. md5(ts+privateKey+publicKey)
+For example, a user with a public key of "1234" and a private key of "abcd" could construct a valid call as follows: http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150 (the hash value is the md5 digest of 1abcd1234)
---
+++ querystring/query_string_marvelmodifier.go
@@ -0,0 +1,75 @@
+package querystring
+
+import (
+	"encoding/json"
+	"net/http"
+  "time"
+  "strconv"
+  "crypto/md5"
+  "encoding/hex"
+
+	"github.com/google/martian"
+	"github.com/google/martian/parse"
+)
+
+func init() {
+	parse.Register("querystring.MarvelModifier", marvelModifierFromJSON)
+}
+
+type MarvelModifier struct {
+	public, private string
+}
+
+type MarvelModifierJSON struct {
+	Public  string               `json:"public"`
+	Private string               `json:"private"`
+	Scope []parse.ModifierType `json:"scope"`
+}
+
+// ModifyRequest modifies the query string of the request with the given key and value.
+func (m *MarvelModifier) ModifyRequest(req *http.Request) error {
+	query := req.URL.Query()
+  ts := strconv.FormatInt(time.Now().Unix(), 10)
+  hash := GetMD5Hash(ts + m.private + m.public)
+  query.Set("apikey", m.public)
+  query.Set("ts", ts)
+  query.Set("hash", hash)
+	req.URL.RawQuery = query.Encode()
+
+	return nil
+}
+
+func GetMD5Hash(text string) string {
+    hasher := md5.New()
+    hasher.Write([]byte(text))
+    return hex.EncodeToString(hasher.Sum(nil))
+}
+
+// NewModifier returns a request modifier that will set the query string
+// at key with the given value. If the query string key already exists all
+// values will be overwritten.
+func MarvelNewModifier(public, private string) martian.RequestModifier {
+	return &MarvelModifier{
+		public:   public,
+		private: private,
+	}
+}
+
+// modifierFromJSON takes a JSON message as a byte slice and returns
+// a querystring.modifier and an error.
+//
+// Example JSON:
+// {
+//  "public": "apikey",
+//  "private": "apikey",
+//  "scope": ["request", "response"]
+// }
+func marvelModifierFromJSON(b []byte) (*parse.Result, error) {
+	msg := &MarvelModifierJSON{}
+
+	if err := json.Unmarshal(b, msg); err != nil {
+		return nil, err
+	}
+
+	return parse.NewResult(MarvelNewModifier(msg.Public, msg.Private), msg.Scope)
+}
